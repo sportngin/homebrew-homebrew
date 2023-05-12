edaf0856c6d37151def98d1948a0c0d456aa5d31
Nick Sieger <nick@nicksieger.com>
2023-05-12 14:50:55 -0500

    Tap mysql-client formula frozen at 8.0.32

---
+++ Formula/mysql-client@8.0.32.rb
@@ -0,0 +1,58 @@
+class MysqlClientAT8032 < Formula
+  desc "Open source relational database management system"
+  homepage "https://dev.mysql.com/doc/refman/8.0/en/"
+  url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.32.tar.gz"
+  sha256 "1a83a2e1712a2d20b80369c45cecbfcc7be9178d4fc0e81ffba5c273ce947389"
+  license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
+
+  livecheck do
+    formula "mysql"
+  end
+
+  keg_only "it conflicts with mysql (which contains client libraries)"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on "libfido2"
+  # GCC is not supported either, so exclude for El Capitan.
+  depends_on macos: :sierra if DevelopmentTools.clang_build_version < 900
+  depends_on "openssl@1.1"
+  depends_on "zlib" # Zlib 1.2.12+
+  depends_on "zstd"
+
+  uses_from_macos "libedit"
+
+  fails_with gcc: "5"
+
+  def install
+    # -DINSTALL_* are relative to `CMAKE_INSTALL_PREFIX` (`prefix`)
+    args = %W[
+      -DFORCE_INSOURCE_BUILD=1
+      -DCOMPILATION_COMMENT=Homebrew
+      -DDEFAULT_CHARSET=utf8mb4
+      -DDEFAULT_COLLATION=utf8mb4_general_ci
+      -DINSTALL_DOCDIR=share/doc/#{name}
+      -DINSTALL_INCLUDEDIR=include/mysql
+      -DINSTALL_INFODIR=share/info
+      -DINSTALL_MANDIR=share/man
+      -DINSTALL_MYSQLSHAREDIR=share/mysql
+      -DWITH_BOOST=boost
+      -DWITH_EDITLINE=system
+      -DWITH_FIDO=system
+      -DWITH_LIBEVENT=system
+      -DWITH_ZLIB=system
+      -DWITH_ZSTD=system
+      -DWITH_SSL=yes
+      -DWITH_UNIT_TESTS=OFF
+      -DWITHOUT_SERVER=ON
+    ]
+
+    system "cmake", ".", *std_cmake_args, *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mysql --version")
+  end
+end
